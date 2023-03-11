89fd5080fe9d80fc8392c3410a5eb9fc3cc5943d
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-03-11 10:17:40 +0700

    feat: adding code for TablePage

--- sources/b-end/src/pages/TablePage.tsx
+++ sources/b-end/src/pages/TablePage.tsx
@@ -1,7 +1,77 @@
+// TODO: tablePage - Table Render (1)
+import { useEffect, useState } from "react";
+
+// TODO: tablePage - Table Render (2)
+type Comment = {
+  id: number;
+  email: string;
+  body: string;
+};
+
 const TablePage = () => {
+  // TODO: tablePage - Table Render (3)
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    // TODO: tablePage - Table Render (4)
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+
+        if (!response.ok) {
+          const body = await response.text();
+          throw new Error(body);
+        }
+
+        // TODO: tablePage - Table Render (5)
+        const responseJson: Comment[] = await response.json();
+        setComments(responseJson);
+      } catch (err) {
+        // TODO: tablePage - Table Render (6)
+        if (typeof err === "string") {
+          console.log(err);
+        }
+      }
+    })();
+  }, []);
+
+  const eachRowButtonDeleteOnClickHandler = (data: Comment) => {
+    console.log(data);
+  };
+
   return (
     <>
       <p>Ini adalah halaman Table</p>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Body</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          // TODO: tablePage - Table Render (7)
+          {comments.map((comment) => (
+            <tr key={comment.id}>
+              <td>{comment.id}</td>
+              <td>{comment.email}</td>
+              <td>{comment.body}</td>
+              <td>
+                <button
+                  onClick={() => eachRowButtonDeleteOnClickHandler(comment)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
